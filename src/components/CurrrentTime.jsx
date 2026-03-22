e4e83c3ee62c531627050773af03a45d9d14e91c
sharmaa-abhi <sharmaa24434@gmail.com>
2026-03-22 22:10:40 +0530

    it changes at currentTime

--- src/components/CurrrentTime.jsx
+++ src/components/CurrrentTime.jsx
@@ -1,10 +1,35 @@
-let CurrentTime = () => {
+// let CurrentTime = () => {
 
-  let time = new Date();
+//   let time = new Date();
+//   return (
+//     <div>
+//       <p>This is curent time:{time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
+//     </div>
+//   );
+// };
+// export default CurrentTime;
+
+import { useState, useEffect } from "react";
+
+let CurrentTime = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date()); // update time every second
+    }, 1000);
+
+    return () => clearInterval(interval); // cleanup
+  }, []);
+
   return (
     <div>
-      <p>This is curent time:{time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
+      <p>
+        This is current time: {time.toLocaleDateString()} -{" "}
+        {time.toLocaleTimeString()}
+      </p>
     </div>
   );
 };
+
 export default CurrentTime;
